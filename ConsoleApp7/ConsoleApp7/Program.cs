3d6ab78383c1fec31236b03eb101fe357776cd3c
Rashad-Taghizada <rashadit@code.edu.az>
2026-02-03 10:20:11 +0100

    commit7.

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,10 @@
+﻿// 8.Verilmiş array-ı tərsinə çap edin.
+//Məsələn: [1, 2, 3, 4] → Çıxış: 4 3 2 1
+
+int[] elementler = { 1, 2, 3, 4 };
+
+Console.Write("Çıxış: ");
+
+for (int i = elementler.Length - 1; i >= 0; i--)
+
+    Console.Write(ededler[i] + " ");
