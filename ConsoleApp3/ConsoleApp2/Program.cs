1feb079ea3872942b7f926187a9f734bdb0084a2
Rashad-Taghizada <rashadit@code.edu.az>
2026-02-03 10:15:59 +0100

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp2/Program.cs
@@ -0,0 +1,5 @@
+﻿// 3.Verilmiş Arrayın ilk və son elementlərinin cəmini tapın
+int[] elementler = { 12, 19, 21, 54, 28 };
+int ilkElement=elementler[0];
+int sonElement=elementler[4];
+Console.WriteLine(ilkElement+ sonElement); 
