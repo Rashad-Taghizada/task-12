22889265c8ac166dc62da5a0ccad24cfc1e10094
Rashad-Taghizada <rashadit@code.edu.az>
2026-02-03 10:21:04 +0100

    coomit8.

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,12 @@
+﻿//9. Verilmiş massivin ən kiçik elementini çıxışa verən proqram yazın
+int[] ededler = { 13, 32, 18, 20, 3, 45 };
+int enKicik = ededler[0];
+for (int i = 1; i < ededler.Length; i++)
+{
+    if (ededler[i] < enKicik)
+    {
+        enKicik = ededler[i];
+    }
+}
+
+Console.WriteLine("En kicik element: " + enKicik);
