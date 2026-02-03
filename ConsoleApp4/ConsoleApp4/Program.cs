484c966b5c50a214943a4aab1a031d4a26b086c3
Rashad-Taghizada <rashadit@code.edu.az>
2026-02-03 10:17:39 +0100

    Add files via upload
    coomit 4.

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿// 5.Verilmiş n tam ədədinin neçə mərtəbəli olduğunu tapın. Məs: 23452, output: 5   
+int n = 23452;
+int a = 0;
+
+
+if (n == 0)
+{
+    a = 1;
+}
+else
+{
+    
+    while (n > 0)
+    {
+        n = n / 10; 
+        a++;   
+    }
+}
+
+Console.WriteLine("Mertebe sayı: " + a);
