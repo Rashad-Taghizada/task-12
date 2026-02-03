5ea7acf8f614c5268ce446e803e89f58d49a8b10
Rashad-Taghizada <rashadit@code.edu.az>
2026-02-03 10:19:17 +0100

    commit6.

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See\Verilmiş array-da müəyyən bir elementin neçə dəfə təkrarlandığını tapın.
+//Məsələn: [1, 2, 3, 2, 4, 2], axtarış: 2 → Çıxış: 3
+int[] ededler = { 1, 2, 3, 2, 4, 2 };
+int axtaris = 2;
+int saygac = 0;
+for (int i = 0; i < ededler.Length; i++)
+{
+    if (ededler[i] == axtaris)
+    {
+        saygac++;
+    }
+}
+Console.WriteLine("Tekrar say: " + saygac);
