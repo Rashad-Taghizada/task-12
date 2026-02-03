55c6515fe8ef6857f8dba28e0095ce2185dd7c64
Rashad-Taghizada <rashadit@code.edu.az>
2026-02-03 10:14:30 +0100

    Add files via upload
    commit 2.

---
+++ ConsoleApp2/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// 2.Verilmiş int Arrayın ən böyük dəyərə sahib elementini tapın
+int[] elementler = { 13, 32, 18, 20, 3 };
+int a = elementler[0];
+for (int i = 1; i < elementler.Length; i++)
+{
+    if (elementler[i] > a)
+    {
+        a = elementler[i];
+    }
+}
+Console.WriteLine(a);
