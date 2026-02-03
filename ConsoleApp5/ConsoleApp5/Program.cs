5c3b94e813a371a4fddb825f8677980b2c094916
Rashad-Taghizada <rashadit@code.edu.az>
2026-02-03 10:18:31 +0100

    commit5.
    commit5.

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,14 @@
+﻿// 6.Tam ədədlər ardıcıllığı verilir. Ardıcıllığın hər bir mənfi olmayan elementini 2 vahid artırmalı.
+int[] ededler = { -5, 10, 0, -2, 15, 3 };
+for (int i = 0; i < ededler.Length; i++)
+{
+    if (ededler[i] >= 0)
+    {
+        ededler[i] = ededler[i] + 2;
+    }
+}
+Console.WriteLine(" ardıcıllıq:");
+for (int i = 0; i < ededler.Length; i++)
+{
+    Console.Write(ededler[i] + " ");
+}
